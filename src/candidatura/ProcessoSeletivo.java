80e905fd51d74565164e5a72ab613a8a1070db69
pgomezzy <palp53412@gmail.com>
2024-09-02 11:55:34 -0300

    Tentando contato com o cliente

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,37 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String [] candidatos = {"FELIPE","MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        for (String candidato : candidatos){
+            entrandoEmContato(candidato);
+        }
+    }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA ");
+        else
+            System.out.println("NÂO CONSEGUIMOS CONTATO COM " + candidato +", NUMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADAS");
+    }
+
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados(){
